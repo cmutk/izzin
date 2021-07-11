3724a16446886b76cd02a50e982c55afbf9aceac
zhunor <ozerorhun@gmail.com>
2021-07-11 18:05:49 +0300

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 
 function App(): JSX.Element {
-  return <div classasd="App">ad as as a</div>;
+  return <div className="App">ad as as a</div>;
 }
 
 export default App;
