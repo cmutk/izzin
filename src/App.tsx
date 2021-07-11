b0909278a0d9fd9735842c809b2cc6b6b0c731b8
zhunor <ozerorhun@gmail.com>
2021-07-11 17:56:11 +0300

    Add eslint prettier and other configurations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+function App(): JSX.Element {
+  return <div className="App">sadasd</div>;
 }
 
 export default App;
