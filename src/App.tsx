798fa4597ebcaa9646e69b5aadc2c4cc32a88324
cmutk <26392038+cmutk@users.noreply.github.com>
2021-07-11 21:00:32 +0300

    Add ant desıng & router - Management & Employee paths added - Links added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 import './App.css';
+import Employee from './views/Employee';
+import Management from './views/Management';
 
 function App(): JSX.Element {
-  return <div className="App">aashdusahudhasidhasiu</div>;
+  const { Header, Content } = Layout;
+  return (
+    <div className="App">
+      <Layout>
+        <Router>
+          <Header>
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+              <Menu.Item key="1">
+                <Link to="/">Employee</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/management">Management</Link>
+              </Menu.Item>
+              <Menu.Item key="3">Profil</Menu.Item>
+            </Menu>
+          </Header>
+          <Content>
+            <Switch>
+              <Route path="/" exact component={Employee}></Route>
+              <Route path="/management" exact component={Management}></Route>
+            </Switch>
+          </Content>
+        </Router>
+      </Layout>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/views/Employee.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Employee(): JSX.Element {
+  return <div>Employee</div>;
+}
+
+export default Employee;
